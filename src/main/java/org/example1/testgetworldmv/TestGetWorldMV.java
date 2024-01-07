4fbec6d0215bb2060b751318435470b14ca2e0e3
Mattstar211 <asafin2006@gmail.com>
2024-01-07 21:43:01 +0300

    add onPlayerCommandPreprocess and getLobbyWorld connect MultiverseCore API

--- src/main/java/org/example1/testgetworldmv/TestGetWorldMV.java
+++ src/main/java/org/example1/testgetworldmv/TestGetWorldMV.java
@@ -1,17 +1,63 @@
 package org.example1.testgetworldmv;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import com.onarandombox.MultiverseCore.MultiverseCore;
+import com.onarandombox.MultiverseCore.api.MultiverseWorld;
 
-public final class TestGetWorldMV extends JavaPlugin {
+import java.util.Objects;
+
+public final class TestGetWorldMV extends JavaPlugin implements  Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        Bukkit.getServer().getPluginManager().registerEvents(this, this);
+        Objects.requireNonNull(getCommand("getWorld")).setExecutor(this);
     }
 
+    @EventHandler
+    public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
+        String[] split = event.getMessage().split(" ");
+        if (event.getPlayer().isOp()) {
+            if (split.length > 0 && split[0].equalsIgnoreCase("/setBorderRadius")) {
+                if (split.length>1){
+                    String nameWorld = split[1];
+                    World world = getLobbyWorld(nameWorld);
+                    if(world != null) event.getPlayer().sendMessage("Мир " + nameWorld + " успешно получен!");
+                    else event.getPlayer().sendMessage("Мир " + nameWorld + " не существует!");
+                }
+            }else event.getPlayer().sendMessage("Использование: /getWorld <имя_мира>");
+        }else event.getPlayer().sendMessage("Недостаточно прав!");
+    }
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
+    public World getLobbyWorld(String name) {
+        // Получаем экземпляр MultiverseCore
+        MultiverseCore multiverseCore = (MultiverseCore) Bukkit.getPluginManager().getPlugin("Multiverse-Core");
+
+        // Проверяем, что MultiverseCore установлен
+        if (multiverseCore == null) {
+            // Обработка ошибки - MultiverseCore не установлен
+            return null;
+        }
+
+        // Получаем мир (lobby) по его имени
+        MultiverseWorld nameWorld = multiverseCore.getMVWorldManager().getMVWorld(name);
+
+        // Проверяем, что мир найден
+        if (nameWorld == null) {
+            // Обработка ошибки - мир не найден
+            return null;
+        }
+
+        // Возвращаем объект World
+        return nameWorld.getCBWorld();
+    }
+
 }
